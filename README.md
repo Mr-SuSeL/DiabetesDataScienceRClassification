17e62f9ecd138bc2dadaa4eb2ae32c46f6583a29
Lukasz Suslowicz <susel.invest@gmail.com>
2024-01-13 15:16:24 +0100

    data imputation zeros replacement by NA's

---
+++ README.md
@@ -0,0 +1,13 @@
+## DIABETES
+
+Data Science about to make classification in dataset diabetes in R language. Diabetes Prediction using kNN
+
+### Dataset from kaggle
+
+[diabetes-data-set](https://www.kaggle.com/datasets/mathchi/diabetes-data-set%5Blink%5D){.uri}
+
+### Libraries
+
+-   ggplot
+-   dplyr
+-   class
--- exc.R
+++ exc.R
@@ -1,1 +1,60 @@
 # https://www.kaggle.com/datasets/mathchi/diabetes-data-set
+library(dplyr)
+library(class) #knn
+library(ggplot2)
+#install.packages('skimr')
+library(skimr)
+
+cukrzyca <- read.csv("diabetes.csv")
+head(cukrzyca)
+
+cukrzyca %>% is.na() %>% sum()
+length(cukrzyca)
+skim(cukrzyca)
+
+# all zeros replace by NA's without Outcome
+df <- cukrzyca[-9] # minus outcome column
+df[df == 0] <- NA
+skim(df)
+
+ # All NA replace by mean value of each column
+for(i in 1:ncol(df)){
+  df[is.na(df[,i]), i] <- mean(df[,i], na.rm = TRUE)
+}
+
+skim(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
